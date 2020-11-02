b703df16966ce548883090b1aceddb6b71c26efa
Ryan Flynn <ryanflynn44@gmail.com>
2020-11-02 10:52:26 -0500

    Set up redux store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const rootReducer = () => {
+  return {notes: []}
+}
+
+const store = createStore(rootReducer)
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App  />
+  </Provider>,
   document.getElementById('root')
 );
 
